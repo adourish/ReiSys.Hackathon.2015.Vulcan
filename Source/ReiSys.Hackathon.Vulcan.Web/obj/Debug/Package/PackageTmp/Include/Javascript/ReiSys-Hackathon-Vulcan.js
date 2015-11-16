6dd28bc89e678d2b209e667c12c6701f056afe38
adourish <adourish@reisystems.com>
2015-11-15 22:16:25 -0500

    layer changes

--- Source/ReiSys.Hackathon.Vulcan.Web/obj/Debug/Package/PackageTmp/Include/Javascript/ReiSys-Hackathon-Vulcan.js
+++ Source/ReiSys.Hackathon.Vulcan.Web/obj/Debug/Package/PackageTmp/Include/Javascript/ReiSys-Hackathon-Vulcan.js
@@ -319,10 +319,10 @@ var ReiSysHackathon;
                     this.disasterColorModelItems = new Array();
                     this.disasterColorModelItems.push(new DisasterColorModel("Hurricane", "#ffffcc", "#CC00FF"));
                     this.disasterColorModelItems.push(new DisasterColorModel("Severe Storm(s)", "#ffffcc", "#FFFF00"));
-                    this.disasterColorModelItems.push(new DisasterColorModel("Fire", "#ffffcc", "#800026"));
+                    this.disasterColorModelItems.push(new DisasterColorModel("Fire", "#6D0019", "#FF003C"));
                     this.disasterColorModelItems.push(new DisasterColorModel("Flood", "#ffffcc", "#0000FF"));
                     this.disasterColorModelItems.push(new DisasterColorModel("Severe Ice Storm", "#ffffcc", "#00FFFF"));
-                    this.disasterColorModelItems.push(new DisasterColorModel("Snow", "#ffffcc", "#000000"));
+                    this.disasterColorModelItems.push(new DisasterColorModel("Snow", "#03588C", "#F0F2F2"));
                     this.disasterColorModelItems.push(new DisasterColorModel("Overall", "#ffffcc", "#FFA200"));
                 }
                 return DisasterColorCollectionModel;
